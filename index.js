c2495077a7c50deb0947462a3083aa17e0700796
Nanofaux <nanofaux@hotmail.com>
2024-12-12 22:50:41 -0500

    Make origin anywhere

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: process.env.NETLIFY_URL || "https://localhost:3000",
+    origin: '*',
 }));
 const sessionOptions = {
     secret: process.env.SESSION_SECRET || "kanbas",
